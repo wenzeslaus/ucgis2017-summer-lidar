23d67526ec68015a46eef0607ba07b9623622727
Vaclav Petras <wenzeslaus@gmail.com>
2017-05-22 14:02:34 -0400

    fix var name,

--- footprints_to_points.sh
+++ footprints_to_points.sh
@@ -10,8 +10,8 @@ DETECTED_FOOTPRINTS=area
 TRUE_FOOTPRINT_ID_COL=building_id
 DETECTED_FOOTPRINT_ID_COL=class_building_cat
 
-v.db.addcolumn $POINTS column="$DETECTED_FOOTPRINT_ID_COL INTEGER"
-v.what.vect map=$POINTS col=$DETECTED_FOOTPRINT_ID_COL query_map=$TRUE_FOOTPRINTS query_column=objectid
+v.db.addcolumn $POINTS column="$TRUE_FOOTPRINT_ID_COL INTEGER"
+v.what.vect map=$POINTS col=$TRUE_FOOTPRINT_ID_COL query_map=$TRUE_FOOTPRINTS query_column=objectid
 
 v.db.addcolumn $POINTS column="$DETECTED_FOOTPRINT_ID_COL INTEGER"
 v.what.vect map=$POINTS col=$DETECTED_FOOTPRINT_ID_COL query_map=$DETECTED_FOOTPRINTS query_column=cat
