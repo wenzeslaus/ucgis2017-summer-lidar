3479548db48e185a10127078b2252a8e7dbccd08
Vaclav Petras <wenzeslaus@gmail.com>
2017-05-23 14:55:14 -0400

    add missing output dir

--- qsub_wget_ny.sh
+++ qsub_wget_ny.sh
@@ -3,12 +3,14 @@
 #PBS -S /bin/bash
 #PBS -m ae
 #PBS -M vpetras@ncsu.edu
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=2:ppn=20
 #PBS -l walltime=0:30:00
 
 module load parallel
 
+export OUTPUT=/projects/ucgis/OpenProblems/data/CyberGISandGeospatialDataScience/DataQualityAndUncertainty/outputs/ny
 export SCRIPTS_DIR=$HOME/scripts/ucgis2017-summer-lidar
 
 parallel < $SCRIPTS_DIR/wget_ny.sh
-parallel < $SCRIPTS_DIR/wget_uzip.sh
+parallel < $SCRIPTS_DIR/uzip_ny.sh
+parallel < $SCRIPTS_DIR/uzip_las_ny.sh
