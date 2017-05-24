f9c9f970c55cd4504c25ce693cf1236a84b75e07
Vaclav Petras <wenzeslaus@gmail.com>
2017-05-24 10:32:50 -0400

    sync NY to JL

--- qsub_classification_ny.sh
+++ qsub_classification_ny.sh
@@ -54,8 +54,8 @@ function classify {
     pdal pipeline $PIPELINE --readers.las.filename="$TMP_POINTS" --writers.las.filename="$OUTPUT_POINTS"
     grass72 -e -c $GRASS_LOCATION/$TMP_MAPSET
     grass72 $GRASS_LOCATION/$TMP_MAPSET --exec $SCRIPTS_DIR/set_region.sh $BASE_POINTS
-    grass72 $GRASS_LOCATION/$TMP_MAPSET --exec $SCRIPTS_DIR/points_to_grass.sh $OUTPUT_POINTS
     grass72 $GRASS_LOCATION/$TMP_MAPSET --exec g.region res=1 -a
+    grass72 $GRASS_LOCATION/$TMP_MAPSET --exec $SCRIPTS_DIR/points_to_grass.sh $OUTPUT_POINTS
     grass72 $GRASS_LOCATION/$TMP_MAPSET --exec $SCRIPTS_DIR/points_to_footprints.sh
     grass72 $GRASS_LOCATION/$TMP_MAPSET --exec $SCRIPTS_DIR/footprints_to_points.sh
     grass72 $GRASS_LOCATION/$TMP_MAPSET --exec $SCRIPTS_DIR/getting_tp_and_fp.sh
